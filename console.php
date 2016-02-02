8966dcf5ea793dacb41eac58aa053bd8d2d3c151
Steven Bühner <buehner@me.com>
2016-02-02 02:25:20 +0100

    Auto Packacking

--- console.php
+++ console.php
@@ -8,6 +8,6 @@ date_default_timezone_set("Europe/Berlin");
 use StevenBuehner\Console\KingofsatCommand;
 use Symfony\Component\Console\Application;
 
-$application = new Application('KingOfSat', '0.1-dev');
+$application = new Application('KingOfSat', '@package_version@');
 $application->add(new KingofsatCommand());
 $application->run();
